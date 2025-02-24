051b114f0f12c58ab4e6ad708195d607c251f83e
Thatguycalledcoder <quarshiedavid62@gmail.com>
2025-02-24 11:27:43 +0000

    Added signup.

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("ff/", include("fblog.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", include("blog.urls")),
 ]
